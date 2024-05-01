0c4ea24d5d8c72622b00ce2ed1a1f94d223e8a99
rembrandt09 test <officialzki2006@gmail.com>
2024-05-01 17:37:41 +0000

    RMX2185: remove localversion_auto this trash commit

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
-export KBUILD_BUILD_HOST="picasso09"
-export KBUILD_BUILD_USER="picasso"
+export KBUILD_BUILD_HOST="picasso09" #edit nama mu
+export KBUILD_BUILD_USER="picasso" #edit nama mu
 export PATH="$(pwd)/clang/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+    make -j$(nproc --all) O=out ARCH=arm64 \
+			  oppo6765_defconfig # masukkan nama defconfig hp mu seperti <device>_defconfig
+    make -j16 ARCH=arm64 O=out \
+                          HOSTCC="ccache clang" \
+                          HOSTCXX="ccache clang++" \
+                          CC="ccache clang" \
+                          LD=ld.lld \
+                          AR=llvm-ar \
+                          NM=llvm-nm \
+                          OBJCOPY=llvm-objcopy \
+                          OBJDUMP=llvm-objdump \
+                          READELF=llvm-readelf \
+                          OBJSIZE=llvm-size \
+                          STRIP=llvm-strip \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
